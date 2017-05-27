13372f00edecbb6399fda3f20164cef96ed4838a
TitanSnow <sweeto@live.cn>
2017-05-27 16:16:07 +0800

    add os.isdir()

--- core/xmos/__init__.py
+++ core/xmos/__init__.py
@@ -2,6 +2,7 @@ from functools import partial
 from xmos.argv import xm_os_argv
 from xmos.find import xm_os_find
 from xmos.uuid import xm_os_uuid
+from xmos.isdir import xm_os_isdir
 
 def register(lua):
     lua.execute("os = os or {}")
@@ -9,3 +10,4 @@ def register(lua):
     xmos.argv = partial(xm_os_argv, lua)
     xmos.find = partial(xm_os_find, lua)
     xmos.uuid = partial(xm_os_uuid, lua)
+    xmos.isdir = partial(xm_os_isdir, lua)
---
+++ core/xmos/isdir.py
@@ -0,0 +1,4 @@
+from os.path import isdir
+
+def xm_os_isdir(lua, ph):
+    return isdir(ph)
