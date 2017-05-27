3d7d07d542984f19ea4d149fd2469569685b55d7
TitanSnow <sweeto@live.cn>
2017-05-27 21:40:49 +0800

    add path.is_absolute()

--- core/xmpath/__init__.py
+++ core/xmpath/__init__.py
@@ -2,6 +2,7 @@ from functools import partial
 from xmpath.relative import xm_path_relative
 from xmpath.absolute import xm_path_absolute
 from xmpath.translate import xm_path_translate
+from xmpath.is_absolute import xm_path_is_absolute
 
 def register(lua):
     lua.execute("path = path or {}")
@@ -9,3 +10,4 @@ def register(lua):
     xmpath.relative = partial(xm_path_relative, lua)
     xmpath.absolute = partial(xm_path_absolute, lua)
     xmpath.translate = partial(xm_path_translate, lua)
+    xmpath.is_absolute = partial(xm_path_is_absolute, lua)
---
+++ core/xmpath/is_absolute.py
@@ -0,0 +1,4 @@
+from os.path import isabs
+
+def xm_path_is_absolute(lua, ph):
+    return isabs(ph)
