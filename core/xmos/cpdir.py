aec11cca0b37699fe25ba4aacfa47a2eac71334e
TitanSnow <sweeto@live.cn>
2017-05-28 16:31:11 +0800

    fix os.cpdir/file()

--- core/xmos/cpdir.py
+++ core/xmos/cpdir.py
@@ -1,11 +1,12 @@
 from shutil import copytree
 from xmerrno import set_errno
 from xmtrace import xmtrace
+from os.path import expanduser
 
 @xmtrace
 def xm_os_cpdir(lua, src, dst):
     try:
-        copytree(src, dst)
+        copytree(expanduser(src), expanduser(dst))
     except OSError as e:
         set_errno(e.errno)
         return False
--- core/xmos/cpfile.py
+++ core/xmos/cpfile.py
@@ -1,10 +1,15 @@
 from shutil import copy2
 from xmerrno import set_errno
 from xmtrace import xmtrace
+from os.path import expanduser, dirname
+from os import makedirs
 
 @xmtrace
 def xm_os_cpfile(lua, src, dst):
     try:
+        src = expanduser(src)
+        dst = expanduser(dst)
+        makedirs(dirname(dst), 0o755, True)
         copy2(src, dst)
     except OSError as e:
         set_errno(e.errno)
