c4584653468b32f3cdbd8a454c4c7e291a43d108
TitanSnow <sweeto@live.cn>
2017-05-28 11:03:06 +0800

    remove trace

--- core/xmtrace.py
+++ core/xmtrace.py
@@ -1,8 +1,2 @@
 def xmtrace(func):
-    def newfunc(lua, *args):
-        try:
-            return func(lua, *args)
-        except Exception:
-            print(lua.globals().debug.traceback())
-            raise
-    return newfunc
+    return func
