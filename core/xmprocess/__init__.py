ef724139ee505d6489874a00ecee6471f7a91d9b
TitanSnow <sweeto@live.cn>
2017-05-28 10:26:10 +0800

    finish module process

--- core/xmprocess/__init__.py
+++ core/xmprocess/__init__.py
@@ -1,2 +1,13 @@
+from functools import partial
+from xmprocess.openv import xm_process_openv
+from xmprocess.wait import xm_process_wait
+from xmprocess.waitlist import xm_process_waitlist
+from xmprocess.close import xm_process_close
+
 def register(lua):
-    pass
+    lua.execute("process = process or {}")
+    xmprocess = lua.globals().process
+    xmprocess.openv = partial(xm_process_openv, lua)
+    xmprocess.wait = partial(xm_process_wait, lua)
+    xmprocess.waitlist = partial(xm_process_waitlist, lua)
+    xmprocess.close = partial(xm_process_close, lua)
