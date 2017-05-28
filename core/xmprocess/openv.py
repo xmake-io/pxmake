2bafd88bc82746f0b9e0d9bb8c014abb799f1c9e
TitanSnow <sweeto@live.cn>
2017-05-28 11:00:27 +0800

    fix process.openv()

--- core/xmprocess/openv.py
+++ core/xmprocess/openv.py
@@ -5,7 +5,7 @@ from xmerrno import set_errno
 @xmtrace
 def xm_process_openv(lua, shellname, argv, outfile = None, errfile = None):
     try:
-        return Popen([shellname] + argv, stdout = open(outfile, "w") if outfile != None else None, stderr = open(errfile, "w") if errfile != None else None)
+        return Popen([shellname] + list(argv.values()), stdout = open(outfile, "w") if outfile != None else None, stderr = open(errfile, "w") if errfile != None else None)
     except OSError as e:
         set_errno(e.errno)
         return None
