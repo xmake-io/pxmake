b570368d5a3370a936cfa308bb49bf236b7999f3
TitanSnow <sweeto@live.cn>
2017-05-28 14:59:07 +0800

    add error handling to process.close()

--- core/xmprocess/close.py
+++ core/xmprocess/close.py
@@ -1,8 +1,15 @@
 from xmtrace import xmtrace
 import xmprocess.procpool as procpool
+from xmerrno import set_errno
 
 @xmtrace
 def xm_process_close(lua, process):
-    procpool.pool[process].terminate()
-    procpool.pool[process] = None
-    return True
+    if not procpool.pool[process]:
+        return False
+    try:
+        procpool.pool[process].terminate()
+        procpool.pool[process] = None
+        return True
+    except OSError as e:
+        set_errno(e.errno)
+        return False
