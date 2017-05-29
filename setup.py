8f23e7010a678e1692b49ceda237ebc131abfee9
TitanSnow <sweeto@live.cn>
2017-05-29 19:03:44 +0800

    primary setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pxmake",
+    version = "2.1.5.dev0",
+    packages = find_packages(),
+    install_requires = ["lupa>=1.4"],
+    author = "TitanSnow",
+    author_email = "tttnns1024@gmail.com",
+    url = "https://github.com/TitanSnow/pxmake",
+    description = "xmake implement on python focuses on reuse python's library and API compatibility",
+    license = "Apache 2.0",
+    python_requires = ">=3.3",
+    entry_points = {
+        "console_scripts": [
+            "pxmake = xmmachine:main"
+        ]
+    }
+)
