8767561c3d99e76a1cafbc462ae81a31a213a081
TitanSnow <sweeto@live.cn>
2017-05-29 13:37:21 +0800

    fix readline.history_list()

--- core/xmreadline/__init__.py
+++ core/xmreadline/__init__.py
@@ -4,6 +4,6 @@ def register(lua):
     lua.execute("readline = readline or {}")
     xmreadline = lua.globals().readline
     xmreadline.readline = input
-    xmreadline.history_list = lambda: lua.table(*[readline.get_history_item(i) for i in range(1, readline.get_current_history_length() + 1)])
+    xmreadline.history_list = lambda: lua.table(*[lua.table(line = readline.get_history_item(i)) for i in range(1, readline.get_current_history_length() + 1)])
     xmreadline.add_history = readline.add_history
     xmreadline.clear_history = readline.clear_history
