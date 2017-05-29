604f9279fa8d5498e2518c10e6449680854c8483
TitanSnow <sweeto@live.cn>
2017-05-29 13:08:59 +0800

    add sandbox.interactive()

--- core/xmsandbox/interactive.py
+++ core/xmsandbox/interactive.py
@@ -1,5 +1,18 @@
 from xmtrace import xmtrace
 
+def xm_sandbox_loadline():
+    lns = [input("> ")]
+    if lns[0][0] == '=':
+        lns[0] = 'return' + lns[0][1:]
+    while lns[-1][-1] == '\\':
+        lns[-1] = lns[-1][:-1]
+        lns.append(input(">> "))
+    return '\n'.join(lns)
+
 @xmtrace
 def xm_sandbox_interactive(lua, instance):
-    print(instance)
+    try:
+        while True:
+            print(lua.execute(xm_sandbox_loadline()))
+    except EOFError:
+        pass
