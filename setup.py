301aea4b590487c92749959866216d91bcde9987
TitanSnow <sweeto@live.cn>
2017-05-29 20:57:51 +0800

    add classifiers

--- setup.py
+++ setup.py
@@ -18,5 +18,17 @@ setup(
         ]
     },
     data_files = [(root, [os.path.join(root, f) for f in files]) for root, dirs, files in os.walk("xmake")],
-    zip_safe = False
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Build Tools',
+    ]
 )
