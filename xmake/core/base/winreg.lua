a416d7adca1de8b16bdc31a8c3c008405af57a2c
TitanSnow <sweeto@live.cn>
2017-06-01 14:10:51 +0800

    deal with no ";" path

--- xmake/core/base/winreg.lua
+++ xmake/core/base/winreg.lua
@@ -6,7 +6,7 @@ function winreg.query(keyname)
     local rootkey_end = keyname:find("\\")
     if not rootkey_end then return nil, string.format("parse pathkey and pathsubkey failed: %s", keyname) end
     local r_value_end = keyname:reverse():find(";")
-    if not r_value_end then return nil, string.format("parse value name failed: %s", keyname) end
+    if not r_value_end then r_value_end = 0 end
     rootkey = keyname:sub(1, rootkey_end - 1)
     if rootkey == "HKEY_CLASSES_ROOT" then rootkey = pywinreg.HKEY_CLASSES_ROOT
     elseif rootkey == "HKEY_CURRENT_CONFIG" then rootkey = pywinreg.HKEY_CURRENT_CONFIG
@@ -16,7 +16,7 @@ function winreg.query(keyname)
     else return nil, string.format("invalid registry path: %s", keyname) end
     local ok, rv = pypcall(function()
         key = pytypecall(pywinreg.OpenKey, {pyint, 0}, rootkey, keyname:sub(rootkey_end + 1, #keyname - r_value_end))
-        val, tp = pywinreg.QueryValueEx(key, keyname:reverse():sub(1, r_value_end - 1):reverse())
+        val, tp = pywinreg.QueryValueEx(key, keyname:reverse():sub(1, math.max(0, r_value_end - 1)):reverse())
         key.Close()
         if tp == pywinreg.REG_SZ then return val end
         if tp == pywinreg.REG_EXPAND_SZ then return pywinreg.ExpandEnvironmentStrings(val) end
