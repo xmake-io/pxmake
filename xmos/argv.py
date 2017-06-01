25efd3ac5ea59b36cbb2ef159e95bcf847454364
TitanSnow <sweeto@live.cn>
2017-06-01 11:55:42 +0800

    fix shell split on Windows

--- xmos/argv.py
+++ xmos/argv.py
@@ -1,7 +1,8 @@
 from shlex import split
 from xmtrace import xmtrace
+import os
 
 @xmtrace
 def xm_os_argv(lua, args):
-    arr = split(args)
+    arr = split(args, posix = os.name == "posix")
     return lua.table(*arr)
